4a63712f8752735d376d235d57da0f3ad589186f
Varun Gupta <varungupta3009@gmail.com>
2019-09-26 16:32:02 +0530

    Updates

---
+++ precache-manifest.9d524ebe6bf4c0cc2d6298e775691d77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1eda11cfd6de7bb81d9db4af28a1c9d",
+    "url": "/dsc-logo-generator/index.html"
+  },
+  {
+    "revision": "4043d724fc5c241d24d5",
+    "url": "/dsc-logo-generator/static/css/main.d578d95f.chunk.css"
+  },
+  {
+    "revision": "d2d1f0c69320149aa5c4",
+    "url": "/dsc-logo-generator/static/js/2.7c256ad3.chunk.js"
+  },
+  {
+    "revision": "4043d724fc5c241d24d5",
+    "url": "/dsc-logo-generator/static/js/main.15c1ecb6.chunk.js"
+  },
+  {
+    "revision": "58e80d8799d6e0ec0589",
+    "url": "/dsc-logo-generator/static/js/runtime-main.1ca663f7.js"
+  }
+]);
